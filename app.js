6a333a94b94bbd5c7f45a6e45f9769bfd03476f5
Satoshi Takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-04-19 19:09:20 +0900

    add cross-origin-header setting

--- app.js
+++ app.js
@@ -57,6 +57,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // ******************* Routing Setting ***************************************
 app.use('/', routes);
