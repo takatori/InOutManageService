1cf02056a39a4068bc561f599acf04743a4fc65f
takatori <tktr3w5e0@gmail.com>
2016-04-25 23:50:00 +0900

    Add account test

---
+++ test/accounts.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// import the mongoose helper utilities
+import utils from './utils'
+import should from 'should'
+import Account from '../models/account'
+
+describe('Accounts: models', () => {
+
+    describe('#create()', () => {
+        it('should create a new Account', done => {
+            // Create a Account object to pass 
+        })
+    })
+})
