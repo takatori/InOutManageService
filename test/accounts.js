6a7063f5cfdc8d8034a3ebea29978dc8d5ec750d
takatori <tktr3w5e0@gmail.com>
2016-04-26 22:42:58 +0900

    Add create account test

--- test/accounts.js
+++ test/accounts.js
@@ -9,7 +9,19 @@ describe('Accounts: models', () => {
 
     describe('#create()', () => {
         it('should create a new Account', done => {
-            // Create a Account object to pass 
+            // Create a Account object to pass
+            const account = {
+                name: 'takatori',
+                password: 'satoshi',
+                icon_image_url: 'https://placeimg.com/640/480/any'
+            };
+
+            
+            Account.create(account, (err, craetedUser) => {
+                // Confirm that an error does not exist
+                should.not.exist(err)
+                done()
+            })
         })
     })
 })
